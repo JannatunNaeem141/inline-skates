0d16814106dd45bfff7fe5ed2d15887f32109f04
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-04-05 14:20:05 +0600

    Rating added successfully

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,20 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Review.css'
 
 const Review = (props) => {
-    const { name, img, description } = props.review;
+    const { name, img, description, rating } = props.review;
     return (
         <div className='review'>
             <div className='avatar'>
                 <img src={img} alt="" />
             </div>
-            <h4>{name}</h4>
-            <p>{description}</p>
+            <div>
+                <h4>{name}</h4>
+                <p className='rating-text'>Rating: <span>{rating}</span><FontAwesomeIcon icon={faStar} className='rating'></FontAwesomeIcon></p>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
